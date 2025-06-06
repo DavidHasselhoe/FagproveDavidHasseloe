182a92b90a910282b60a52c19eda3e739f875171
David Hasselø <david.hasselo@omega365.com>
2025-06-06 10:49:49 +0200

    Added routes for updating and deleting tours

--- server/routes/tours.routes.js
+++ server/routes/tours.routes.js
@@ -5,6 +5,7 @@ const auth = require("../middleware/auth.middleware");
 
 router.post("/tours", auth, toursController.createTour);
 router.get("/me/tours", auth, toursController.getMyTours);
-
+router.put("/tours/:id", auth, toursController.updateTour);
+router.delete("/tours/:id", auth, toursController.deleteTour);
 
 module.exports = router;
