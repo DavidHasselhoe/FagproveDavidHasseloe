3de86509e92a1060d2682bd7d4b159f21ddd3798
David Hasselø <david.hasselo@omega365.com>
2025-06-10 11:45:52 +0200

    Add router for getting active competition

--- server/routes/competitions.routes.js
+++ server/routes/competitions.routes.js
@@ -1,10 +1,10 @@
-// competitions.routes.js
 const express = require("express");
 const router = express.Router();
 const competitionsController = require("../controllers/competitions.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
-router.get("/", authMiddleware, competitionsController.getCompetitions);
+router.get("/", competitionsController.getCompetitions);
+router.get("/active", competitionsController.getActiveCompetitions);
 router.post("/", authMiddleware, competitionsController.createCompetition);
 router.put("/:id", authMiddleware, competitionsController.updateCompetition);
 router.delete("/:id", authMiddleware, competitionsController.deleteCompetition);
