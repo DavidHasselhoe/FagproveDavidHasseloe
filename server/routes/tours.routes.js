07389ad967f78b96b134ec5ca0b5810e05bfccd0
David Hasselø <david.hasselo@omega365.com>
2025-06-11 14:27:56 +0200

    Add get for specific tour id

--- server/routes/tours.routes.js
+++ server/routes/tours.routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const toursController = require("../controllers/tours.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
-router.post("/", authMiddleware, toursController.createTour);
 router.get("/", authMiddleware, toursController.getMyTours);
+router.get("/:id", authMiddleware, toursController.getTour);
+router.post("/", authMiddleware, toursController.createTour);
 router.put("/:id", authMiddleware, toursController.updateTour);
 router.delete("/:id", authMiddleware, toursController.deleteTour);
+
 module.exports = router;
